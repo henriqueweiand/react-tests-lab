4a2263de6d0a1b5278099d8d94847e24de5fcf55
henrique weiand <henriqueweiand@gmail.com>
2023-01-31 09:51:17 -0300

    upd

--- how-to-test-react-components/components/SearchBox/__tests__/SearchBox-unit.test.tsx
+++ how-to-test-react-components/components/SearchBox/__tests__/SearchBox-unit.test.tsx
@@ -1,14 +1,32 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import SearchBox from '@/components/SearchBox';
 
 describe('SearchBox', () => {
-  it('Should render the SearchBox', () => {
-    const requestSearch = jest.fn();
+  const requestSearch = jest.fn();
 
+  it('Should render the SearchBox', () => {
     const { getByTestId, queryByPlaceholderText } = render(<SearchBox requestSearch={requestSearch} />)
 
     expect(queryByPlaceholderText('Search')).toBeTruthy();
     expect(getByTestId('search-button')).toBeTruthy();
+  })
+
+  it('Should update the input value', () => {
+    const { queryByPlaceholderText } = render(<SearchBox requestSearch={requestSearch} />)
+    const searchInput = queryByPlaceholderText('Search');
+
+    fireEvent.change(searchInput, { target: { value: "test" } })
+
+    expect(searchInput.value).toBe("test")
+  })
+
+  it('Should update the input value', () => {
+    const { queryByPlaceholderText } = render(<SearchBox requestSearch={requestSearch} />)
+    const searchInput = queryByPlaceholderText('Search');
+
+    fireEvent.change(searchInput, { target: { value: "test" } })
+
+    expect(searchInput.value).toBe("test")
   })
 })
