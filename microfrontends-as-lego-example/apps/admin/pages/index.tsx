c6b4d2a57a18ac0ee1e7b761df92b44412cb73d9
henrique weiand <henriqueweiand@gmail.com>
2023-03-17 22:28:06 -0300

    add base project

---
+++ microfrontends-as-lego-example/apps/admin/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { NextPage } from "next";
+import { Button } from "ui";
+
+const Admin: NextPage = () => {
+  return (
+    <div>
+      <h1>Admin</h1>
+      <Button>Click Me!</Button>
+    </div>
+  );
+};
+
+export default Admin;
---
+++ microfrontends-as-lego-example/packages/shared/ui/Button.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export const Button = ({ children }: any) => {
+    return (
+        <>
+            dale
+            <button>{children}</button>
+        </>
+    );
+};
