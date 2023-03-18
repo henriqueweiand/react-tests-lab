ad7556399229d6d62fc97b999c05abff89c8f2c3
henrique weiand <henriqueweiand@gmail.com>
2023-03-18 13:34:05 -0300

    added husky; config directory;

--- microfrontends-as-lego-example/packages/shared/ui/Button.tsx
+++ microfrontends-as-lego-example/packages/shared/ui/Button.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 export const Button = ({ children }: any) => {
     return (
         <>
-            dale
             <button>{children}</button>
         </>
     );
